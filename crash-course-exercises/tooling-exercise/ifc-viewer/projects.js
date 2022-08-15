1afcbd6d89c26ab57428011481ab209847bf6a89
Gorgious <nathan.hild@gmail.com>
2022-08-15 20:18:26 +0200

    Move models around

--- crash-course-exercises/tooling-exercise/ifc-viewer/projects.js
+++ crash-course-exercises/tooling-exercise/ifc-viewer/projects.js
@@ -4,7 +4,7 @@ for (let i = 1; i < 11; i++) {
     projects.push({
         id: i,
         name: `Model ${i}`,
-        path: i < 10 ? `../static/models/0${i}.ifc` : `../static/models/${i}.ifc`,
+        path: i < 10 ? `../../..//models/0${i}.ifc` : `../../..//models/${i}.ifc`,
         snapshot: "../static/logo-ifcjs.svg",
     });
 }
